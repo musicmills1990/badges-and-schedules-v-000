2b75097062b6407d8484a580d70cbee2393a4aa5
Marc Mills <mdmills1990@gmail.com>
2018-08-20 20:25:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
     "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array.each do |names|
+def batch_badge_creator(attendees)
+  attendees.each do |names|
     "Hello, my name is #{names}."
   end
 end
